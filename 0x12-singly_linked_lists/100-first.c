ae5f6ea7b106ae4508f7849f7a7f6489e2f8bf5d
Rawannm <rawanm060@gmail.com>
2023-07-28 14:04:13 -0700

    linked listed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * function - prints the sentence
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
