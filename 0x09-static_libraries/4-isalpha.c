0d71e8f9de1371a2edc27bd63e6c1aeb1ec94202
Rawannm <rawanm060@gmail.com>
2023-09-01 16:31:06 -0700

    static library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+		
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
